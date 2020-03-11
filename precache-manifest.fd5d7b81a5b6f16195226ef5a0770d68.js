0c291e67778f75a9406a08c9a2a3d79d22ab6f49
edzi <55710055+EDZ1js@users.noreply.github.com>
2020-03-11 01:17:46 +0100

    Updates

---
+++ precache-manifest.fd5d7b81a5b6f16195226ef5a0770d68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8171992102fcc1d4d52f895a19186a90",
+    "url": "/Marvel-comics/index.html"
+  },
+  {
+    "revision": "5bdf636459db5a938238",
+    "url": "/Marvel-comics/static/css/main.d4d0f2d8.chunk.css"
+  },
+  {
+    "revision": "60f4733a41565523826c",
+    "url": "/Marvel-comics/static/js/2.2291dc46.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/Marvel-comics/static/js/2.2291dc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bdf636459db5a938238",
+    "url": "/Marvel-comics/static/js/main.0367d878.chunk.js"
+  },
+  {
+    "revision": "a1c44e41267cb79a844b",
+    "url": "/Marvel-comics/static/js/runtime-main.06e10fe2.js"
+  },
+  {
+    "revision": "0c97c74ff490d843d5c7142908ff2360",
+    "url": "/Marvel-comics/static/media/marvel.0c97c74f.png"
+  }
+]);
